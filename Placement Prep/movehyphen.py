0d434d2577cb0c7da5b2be4ea4aceb0885bea0aa
Rohit Raj Singh <therohitsinghr@gmail.com>
2021-07-04 12:39:08 +0530

    hyphen

---
+++ Placement Prep/movehyphen.py
@@ -0,0 +1,12 @@
+def movehyphen(str):
+    hyphen=""
+    word = ""
+    for i in str:
+        if i == "-":
+            hyphen+=i
+        else:
+            word+=i
+    print(hyphen,word)
+
+str = "Move-Hyphens-to-Front"
+movehyphen(str)
