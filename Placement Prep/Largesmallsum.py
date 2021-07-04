6a17ea97e7086f8ecf0da814bcbf251f89042dc2
Rohit Raj Singh <therohitsinghr@gmail.com>
2021-07-04 12:06:27 +0530

    commit

--- Placement Prep/Largesmallsum.py
+++ Placement Prep/Largesmallsum.py
@@ -1,3 +1,12 @@
+#def LargeSmallSum(arr)
+
+#The function accepts an integers arr of size ’
+# length’ as its arguments you are required to return the 
+# sum of second largest largest element from the even 
+# positions and second smallest from the odd position of 
+# given ‘arr’
+
+
 def LargeSmallSum(a):
     n = len(a)
     eve = []
---
+++ Placement Prep/productsmallpair.py
@@ -0,0 +1,24 @@
+#def ProductSmallestPair(sum, arr)
+
+#The function accepts an integers sum and an 
+# integer array arr of size n. Implement the 
+# function to find the pair, 
+# (arr[j], arr[k]) where j!=k, 
+# Such that arr[j] and arr[k] are the least two elements of 
+# array (arr[j] + arr[k] <= sum) and return the product of 
+# element of this pair
+
+def ProductSmallestPair(sum, a):
+    n = len(a)
+    for j in range(n):
+        for k in range(j+1,n):
+            if(a[j]+a[k]<=sum):
+                print(a[j*a[k]])
+                break
+            else:
+                return 0    
+n = int(input())
+sum = int(input())
+a = list(map(int,input().split()))
+
+
