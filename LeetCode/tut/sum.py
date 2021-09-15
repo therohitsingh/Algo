0ae37ca25db0b3ff8e286c0e1a4ca2668eea1bc6
Rohit Raj Singh <therohitsinghr@gmail.com>
2021-09-15 15:55:23 +0530

    commit

--- LeetCode/tut/sum.py
+++ LeetCode/tut/sum.py
@@ -1,2 +1,15 @@
-a = 11.8
-print(type(a))
+n1 = int(input())
+a = []
+for i in range(n1):
+    a.append(input())
+n2 = int(input())
+b = []
+for j in range(n2):
+    b.append(input())
+
+a = "".join(a)
+b = "".join(b)
+c = int(b)-int(a)
+c = str(c)
+for i in c:
+    print(i)
